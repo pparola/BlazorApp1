064b69150bf0ceda7da5ff53546ef3b5ef2ca565
Pablo <p.parola@gmail.com>
2022-09-21 11:17:36 -0300

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Models/Tarea.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Tarea
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Completada { get; set; }
+
+        public Tarea()
+        {
+            Id = string.Empty;
+            Nombre = string.Empty;
+            Completada = false;
+        }
+    }
+}
---
+++ BlazorApp1/Servicios/TareasServicio.cs
@@ -0,0 +1,50 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1.Servicios
+{
+
+    public interface ITareasServicio
+    {
+        public List<Tarea> Listar();
+        public void Agregar(Tarea tarea);
+    }
+
+
+    public class TareasServicio: ITareasServicio
+    {
+
+        private List<Tarea> Tareas = new List<Tarea>();
+
+        public TareasServicio()
+        {
+
+            Tarea T = new Tarea();
+
+            T.Id = Guid.NewGuid().ToString();
+            T.Nombre = "Aprender Blazor";
+            T.Completada = false;
+
+            Tareas.Add(T);
+
+            T = new Tarea();
+
+            T.Id = Guid.NewGuid().ToString();
+            T.Nombre = "Aprender Bootstrap";
+            T.Completada = false;
+            Tareas.Add(T);
+        }
+
+        public List<Tarea> Listar()
+        {
+            return Tareas;
+        }
+
+
+        public void Agregar(Tarea tarea)
+        {
+            tarea.Id = Guid.NewGuid().ToString();
+            Tareas.Add(tarea);
+        }
+
+    }
+}
